e58e7fe02d98b6482e51f839c7f81117c961a497
Yang2635 <mail@isisy.com>
2023-06-12 23:25:26 +0800

    Update _worker.js
    Add some features

--- _worker.js
+++ _worker.js
@@ -2,61 +2,157 @@ const TELEGRAPH_URL = 'https://api.openai.com';
 const page = `
 <!DOCTYPE html>
 <html lang="zh-cmn-Hans">
-<head>
-    <meta charset="UTF-8">
-    <title>ChatGPT API Proxy</title>
-    <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1"/>
-    <style>
-        html, body{
-            height: 100%;
-        }
+ <head> 
+  <meta charset="UTF-8" /> 
+  <title>ChatGPT API Proxy</title> 
+  <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1" /> 
+  <!-- import element-ui CSS --> 
+  <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css" /> 
+  <!-- import Vue before Element --> 
+  <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script> 
+  <!-- import element-ui JavaScript --> 
+  <script src="https://unpkg.com/element-ui/lib/index.js"></script> 
+  <style>
+  html, body{
+    height: 100%;
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
 
-        body{
-            color: #333;
-            margin: auto;
-            padding: 1em;
-            display: table;
-            box-sizing: border-box;
-            font: lighter 20px "微软雅黑";
-        }
-        a{
-            color: #3498db;
-            text-decoration: none;
-        }
-        h1{
-            margin-top: 0;
-            font-size: 3.5em;
-        }
-        main{
-            margin: 0 auto;
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
+  body{
+    color: #333;
+    margin: 0;
+    padding: 0;
+    font: lighter 20px "微软雅黑";
+  }
+  
+  p {
+    margin-bottom: 0px;
+  }
 
-        .btn{
-            color: #fff;
-            padding: .75em 1em;
-            background: #3498db;
-            border-radius: 1.5em;
-            display: inline-block;
-            transition: opacity .3s, transform .3s;
-        }
-        .btn:hover{
-            transform: scale(1.1);
-        }
-        .btn:active{
-            opacity: .7;
-        }
-    </style>
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 1;
+    width: 100%;
+    max-width: 100%;
+    margin: 0 auto;
+    padding: 0 1em;
+    box-sizing: border-box;
+  }
+
+  footer {
+    width: 100%;
+    margin: auto auto 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 1em;
+    box-sizing: border-box;
+    flex-direction: column;
+    margin-top: auto;
+    padding-bottom: 20px;
+  }
+
+  footer p {
+    font-size: 1rem;
+  }
+
+  h1{
+    margin-top: 0;
+    font-size: 3.5em;
+  }
+
+  .btn{
+    color: #fff;
+    padding: .75em 1em;
+    background: #3498db;
+    border-radius: 1.5em;
+    display: inline-block;
+    transition: opacity .3s, transform .3s;
+  }
+  
+  .btn:hover{
+    transform: scale(1.1);
+  }
+  
+  .btn:active{
+    opacity: .7;
+  }
+
+  a {
+    color: #0077cc;
+    text-decoration: none;
+    background-color: #f0f0f0;
+    padding: 3px 6px;
+    border-radius: 3px;
+    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+  }
+
+  a:hover {
+    background-color: #ddd;
+  }
+</style>
 </head>
-<body>
-<main>
-    <h1>ChatGPT API Proxy</h1>
-    <p>基于 CloudFlare Pages 部署的 ChatGPT API 代理服务。</p>
-    <p>ChatGPT API proxy service deployed on CloudFlare Pages.</p>
-    <p>将域名 https://api.openai.com 替换成 <strong>https://openai.api.isisy.com</strong> 即可体验。</p>
-</main>
+
+<body> 
+  <div style="display: flex; flex-direction: column; height: 100%;">
+    <main style="flex-grow: 1;">
+      <h1>ChatGPT API Proxy</h1> 
+      <p>基于 CloudFlare Pages 部署的 ChatGPT API 代理服务。</p> 
+      <p>ChatGPT API Proxy service deployed on CloudFlare Pages.</p> 
+      <div id="app"> 
+        <p>将域名 https://api.openai.com 替换成 <strong><a onclick="copyToClipboard('https://openai.api.isisy.com')" @click="showMessage">https://openai.api.isisy.com</a></strong> 即可体验。</p> 
+      </div> 
+    </main> 
+    <footer> 
+      <p id="aipinfo"></p> 
+      <p id="ipinfo"></p> 
+      <p id="googleipinfo"></p> 
+    </footer> 
+  </div>
+  <script>
+    new Vue({
+      el: '#app',
+      methods: {
+        showMessage() {
+          this.$message({
+            message: '文本复制成功！',
+            type: 'success'
+          });
+        }
+      }
+    });
+    fetch("https://forge.speedtest.cn/api/location/info")
+      .then(response => response.json())
+      .then(res => {
+        var ipinfo = "你的 IP 地址是: " + res.ip + " " + res.province + res.city + res.distinct + " " + res.net_str;
+        document.getElementById('ipinfo').textContent = ipinfo;
+      })
+      .catch(error => console.error(error));
+	  
+    fetch('https://ipkk.herokuapp.com')
+      .then(response => response.json())
+      .then(res => {
+        var googleipinfo = "访问 Google 的 IP 是:" + res.ip + " " + res.addr + res.info;
+        document.getElementById('googleipinfo').textContent = googleipinfo;
+      })
+      .catch(error => console.error(error));
+	  
+    function copyToClipboard(text) {
+      const input = document.createElement('textarea');
+      input.style.position = 'fixed';
+      input.style.opacity = 0;
+      input.value = text;
+      document.body.appendChild(input);
+      input.select();
+      document.execCommand('Copy');
+      document.body.removeChild(input);
+    }
+  </script>   
 </body>
 </html>
 
